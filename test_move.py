20ee81111c7fc8cc896f829839be2f4407515cc6
Reakain <reakain@users.noreply.github.com>
2022-01-10 12:53:54 -0800

    added first blush at three point turn

--- test_move.py
+++ test_move.py
@@ -7,7 +7,25 @@ import time
 
 #def parallel_parking(side = "left"):
 
-#def three_point_turn(initial = "left"):
+def three_point_turn(initial = "left"):
+    current_dir = 1
+    speed = 100
+    angle = 90
+    if initial == "right":
+        current_dir = -1
+    px.drive(speed, current_dir*angle)
+    time.sleep(0.5)
+    px.stop()
+    current_dir = -1 * current_dir
+    px.drive(-speed, current_dir*angle)
+    time.sleep(0.5)
+    px.stop()
+    current_dir = -1 * current_dir
+    px.drive(speed, current_dir*angle)
+    time.sleep(0.5)
+    px.drive(speed,0)
+    px.stop()
+
 
 
 
