1e614464e447cab56235d7a146ddec5a240ad765
Reakain <reakain@users.noreply.github.com>
2022-01-12 11:59:42 -0800

    added parrallel maybe, set up for calibration

--- test_move.py
+++ test_move.py
@@ -5,14 +5,31 @@ import sys
 from picarx_improved import Picarx
 import time
 
-#def parallel_parking(side = "left"):
-
-def three_point_turn(initial = "left"):
+def parallel_parking(side = "left"):
     current_dir = 1
     speed = 100
-    angle = 90
+    angle = 45
+    #if initial == "right":
+    #    current_dir = -1
+    px.drive(-speed, angle)
+    time.sleep(0.5)
+    px.stop()
+    px.drive(-speed, -angle)
+    time.sleep(0.5)
+    px.stop()
+    px.drive(speed, 20)
+    time.sleep(0.5)
+    px.stop()
+    px.drive(speed, 0)
+    time.sleep(0.5)
+    px.stop()
+
+def three_point_turn(initial = "left"):
+    current_dir = -1
+    speed = 100
+    angle = 45
     if initial == "right":
-        current_dir = -1
+        current_dir = 1
     px.drive(speed, current_dir*angle)
     time.sleep(0.5)
     px.stop()
@@ -32,38 +49,42 @@ def three_point_turn(initial = "left"):
 if __name__ == "__main__":
     try:
         px = Picarx()
-        px.forward(30)
-        time.sleep(0.5)
-        for angle in range(0,35):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
-        for angle in range(35,-35,-1):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)        
-        for angle in range(-35,0):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
-        px.forward(0)
+        px.drive(30,0)
         time.sleep(1)
+        px.stop()
+        
+        # px.forward(30)
+        # time.sleep(0.5)
+        # for angle in range(0,35):
+        #     px.set_dir_servo_angle(angle)
+        #     time.sleep(0.01)
+        # for angle in range(35,-35,-1):
+        #     px.set_dir_servo_angle(angle)
+        #     time.sleep(0.01)        
+        # for angle in range(-35,0):
+        #     px.set_dir_servo_angle(angle)
+        #     time.sleep(0.01)
+        # px.forward(0)
+        # time.sleep(1)
 
-        for angle in range(0,35):
-            px.set_camera_servo1_angle(angle)
-            time.sleep(0.01)
-        for angle in range(35,-35,-1):
-            px.set_camera_servo1_angle(angle)
-            time.sleep(0.01)        
-        for angle in range(-35,0):
-            px.set_camera_servo1_angle(angle)
-            time.sleep(0.01)
-        for angle in range(0,35):
-            px.set_camera_servo2_angle(angle)
-            time.sleep(0.01)
-        for angle in range(35,-35,-1):
-            px.set_camera_servo2_angle(angle)
-            time.sleep(0.01)        
-        for angle in range(-35,0):
-            px.set_camera_servo2_angle(angle)
-            time.sleep(0.01)
+        # for angle in range(0,35):
+        #     px.set_camera_servo1_angle(angle)
+        #     time.sleep(0.01)
+        # for angle in range(35,-35,-1):
+        #     px.set_camera_servo1_angle(angle)
+        #     time.sleep(0.01)        
+        # for angle in range(-35,0):
+        #     px.set_camera_servo1_angle(angle)
+        #     time.sleep(0.01)
+        # for angle in range(0,35):
+        #     px.set_camera_servo2_angle(angle)
+        #     time.sleep(0.01)
+        # for angle in range(35,-35,-1):
+        #     px.set_camera_servo2_angle(angle)
+        #     time.sleep(0.01)        
+        # for angle in range(-35,0):
+        #     px.set_camera_servo2_angle(angle)
+        #     time.sleep(0.01)
             
     finally:
         px.forward(0)
