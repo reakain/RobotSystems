7985a6c4bd90ffe9593d977e30a16f80d330d504
Reakain <reakain@user.noreply.github.com>
2022-03-10 22:13:34 +0000

    cleanup

--- armProject/setup.sh
+++ armProject/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Copying parrot code to ROS workspace"
 cp -R ~/RobotSystems/armProject/parrot ~/armpi_fpv/src/
+echo "Copying launch file to armpi_fpv_bringup"
 cp ~/RobotSystems/armProject/start_functions.launch ~/armpi_fpv/src/armpi_fpv_bringup/launch/
+echo "Building ROS workspace"
 cd ~/armpi_fpv && catkin_make
+echo "Sourcing workspace"
 source ~/armpi_fpv/devel/setup.bash
+echo "Roslaunch test"
 roslaunch /home/ubuntu/armpi_fpv/src/armpi_fpv_bringup/launch/start_functions.launch
