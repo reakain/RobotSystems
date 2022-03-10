4b927030517cb79979b128f5a467935647daf8fb
Reakain <reakain@users.noreply.github.com>
2022-03-10 14:05:30 -0800

    let's make some tweaks

--- armProject/parrot/scripts/parrot.py
+++ armProject/parrot/scripts/parrot.py
@@ -16,7 +16,7 @@ from sensor_msgs.msg import Image
 
 from sensor.msg import Led
 from warehouse.msg import Grasp
-#from parrot.srv import *
+from object_sorting.srv import *
 from hiwonder_servo_msgs.msg import MultiRawIdPosDur
 
 # Our custom classes for analysis and movement
@@ -145,9 +145,9 @@ def heartbeat_srv_cb(msg):
     return rsp
 
 
-th = threading.Thread(target=move)
-th.setDaemon(True)
-th.start()
+#th = threading.Thread(target=move)
+#th.setDaemon(True)
+#th.start()
 
 def run(img):
     global __findingFace
