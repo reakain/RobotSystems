c69a0f203df1dce9b8a99306661e14eb9b9bc071
Reakain <reakain@users.noreply.github.com>
2022-03-11 18:16:30 -0800

    oops, forgot a tihng

--- armProject/gifter/scripts/gifter.py
+++ armProject/gifter/scripts/gifter.py
@@ -547,6 +547,8 @@ def move():
                             bus_servo_control.set_servos(joints_pub, 1500, (
                                 (1, 400), (2, 500), (3, servo_data['servo3']), (4, servo_data['servo4']), (5, servo_data['servo5']),
                                 (6, servo_data['servo6'])))
+                            __startSearch = False
+                            rosply.sleep(.5)
                         elif start_greet:
                             start_greet = False                
                             action_finish = False
